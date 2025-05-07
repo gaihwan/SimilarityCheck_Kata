a9272a828a51cbea882c8a9719587e9c2b011d97
Janghwan.K <gaihwan@gmail.com>
2025-05-07 10:24:31 +0900

    getLengthSimilarrity() 함수 구현 및 UnitTest Code 작성

--- SimilarityCheck_Kata/SimilarityChecker.cpp
+++ SimilarityCheck_Kata/SimilarityChecker.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 class SimilarityChecker {
+public:
+    static const int MAX_POINT = 60;
 
+    int getLengthSimilarity(const string inputA, const string inputB) const {
+        int lengthA = inputA.length();
+        int lengthB = inputB.length();
+        int gab = inputA.length() - inputB.length();
+
+        if (gab == 0) return MAX_POINT;
+        if ((lengthA >= lengthB * 2) || (lengthB >= lengthA * 2)) return 0;
+        if (gab > 0) return MAX_POINT - (gab * MAX_POINT) / lengthB;
+        else return MAX_POINT + (gab * MAX_POINT) / lengthA;
+    }
 };
--- SimilarityCheck_Kata/SimilarityChecker_Test.cpp
+++ SimilarityCheck_Kata/SimilarityChecker_Test.cpp
@@ -5,3 +5,25 @@ TEST(TS, TC1) {
     SimilarityChecker c;
     EXPECT_EQ(1, 1);
 }
+
+TEST(TS, SimilarityCheckerTC1) {
+    SimilarityChecker c;
+    int expected = 40;
+    int actual = c.getLengthSimilarity("ABCD", "ABC");
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(TS, SimilarityCheckerTC2) {
+    SimilarityChecker c;
+    int expected = 60;
+    int actual = c.getLengthSimilarity("BCD", "ABC");
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(TS, SimilarityCheckerTC3) {
+    SimilarityChecker c;
+    int expected = 0;
+    int actual = c.getLengthSimilarity("BCDEFG", "ABC");
+    EXPECT_EQ(expected, actual);
+}
+
